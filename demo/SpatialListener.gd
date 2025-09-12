1dfbf0feaa0db83b608a09c266eed26f5f397769
Clément Rivaille <rivaille.clement@orange.fr>
2025-09-13 00:08:01 +0200

    feat(demo): move speaker

--- demo/SpatialListener.gd
+++ demo/SpatialListener.gd
@@ -1,8 +1,13 @@
 extends Node2D
 class_name SpatialListener
 
+@export var locked := false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if locked:
+		return
+
 	var cursor_pos := get_viewport().get_mouse_position()
 	var pos := Vector2(cursor_pos.x - get_viewport().size.x / 2,
 		cursor_pos.y - get_viewport().size.y / 2)
--- demo/SpatialSpeaker.gd
+++ demo/SpatialSpeaker.gd
@@ -1,7 +1,9 @@
 extends Node2D
 class_name SpatialSpeaker
 
-@onready var sampler: SamplerInstrument = $Sampler
+@export var moving := false
+
+@onready var sampler: Sampler = $Sampler
 
 var note_idx := 0
 var notes: Array[String] = ["C", "E", "G", "A"]
@@ -10,3 +12,12 @@ func play_note():
 	var note := notes[note_idx]
 	sampler.play_note(note)
 	note_idx = (note_idx + 1) % notes.size()
+
+func _process(delta: float) -> void:
+	if !moving:
+		return
+
+	var cursor_pos := get_viewport().get_mouse_position()
+	var pos := Vector2(cursor_pos.x - get_viewport().size.x / 2,
+		cursor_pos.y - get_viewport().size.y / 2)
+	set_global_position(pos)
